0d2f5caecbc912b13580777e881ee6fd5fb0a9e8
Jayandra_Gautam <Jayandragtm@gmail.com>
2017-12-02 19:29:28 +0530

    Commit Chnges

--- LibraryMangnmentTool/src/db/DBConfig.java
+++ LibraryMangnmentTool/src/db/DBConfig.java
@@ -1,8 +1,24 @@
 package db;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+
 import model.UserModel;
 
 public class DBConfig {
+	
+	public static SessionFactory sessionfactory;
+	
+	static{
+		try{
+		sessionfactory = new AnnotationConfiguration().configure().buildSessionFactory();
+			
+		}
+		catch(Exception e){
+			System.out.println("Exception");
+			System.out.println(e.getMessage());
+		}
+	}
 	
 	public boolean saveUserDetail(UserModel userModel){
 		return true;
